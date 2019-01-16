33804dd8e27d287aee572fa1262a05396e6067fd
Your Name <you@example.com>
2019-01-16 14:57:34 +0800

    version 1.0

---
+++ Register1024/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2019/1/15
+ * Time: 13:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Register1024
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label 啊;
+		private System.Windows.Forms.TextBox textBox_url;
+		private System.Windows.Forms.TextBox textBox_code;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 使用方法ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem1;
+		private System.Windows.Forms.TextBox textBox_log;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.啊 = new System.Windows.Forms.Label();
+			this.textBox_url = new System.Windows.Forms.TextBox();
+			this.textBox_code = new System.Windows.Forms.TextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.使用方法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.关于ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.textBox_log = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(21, 56);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(204, 24);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "开始破解";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(21, 29);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "1024地址";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// 啊
+			// 
+			this.啊.Location = new System.Drawing.Point(340, 25);
+			this.啊.Name = "啊";
+			this.啊.Size = new System.Drawing.Size(53, 23);
+			this.啊.TabIndex = 2;
+			this.啊.Text = "邀请码";
+			this.啊.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBox_url
+			// 
+			this.textBox_url.Location = new System.Drawing.Point(100, 29);
+			this.textBox_url.Name = "textBox_url";
+			this.textBox_url.Size = new System.Drawing.Size(175, 21);
+			this.textBox_url.TabIndex = 3;
+			this.textBox_url.Text = "http://t66y.com";
+			// 
+			// textBox_code
+			// 
+			this.textBox_code.Location = new System.Drawing.Point(399, 29);
+			this.textBox_code.Name = "textBox_code";
+			this.textBox_code.Size = new System.Drawing.Size(175, 21);
+			this.textBox_code.TabIndex = 4;
+			this.textBox_code.Text = "cbf1c96e6894{i}7{c}9";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.关于ToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(757, 25);
+			this.menuStrip1.TabIndex = 5;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// 关于ToolStripMenuItem
+			// 
+			this.关于ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.使用方法ToolStripMenuItem,
+			this.关于ToolStripMenuItem1});
+			this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+			this.关于ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+			this.关于ToolStripMenuItem.Text = "使用手册";
+			// 
+			// 使用方法ToolStripMenuItem
+			// 
+			this.使用方法ToolStripMenuItem.Name = "使用方法ToolStripMenuItem";
+			this.使用方法ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+			this.使用方法ToolStripMenuItem.Text = "使用方法";
+			this.使用方法ToolStripMenuItem.Click += new System.EventHandler(this.使用方法ToolStripMenuItemClick);
+			// 
+			// 关于ToolStripMenuItem1
+			// 
+			this.关于ToolStripMenuItem1.Name = "关于ToolStripMenuItem1";
+			this.关于ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+			this.关于ToolStripMenuItem1.Text = "关于";
+			this.关于ToolStripMenuItem1.Click += new System.EventHandler(this.关于ToolStripMenuItem1Click);
+			// 
+			// textBox_log
+			// 
+			this.textBox_log.Location = new System.Drawing.Point(21, 96);
+			this.textBox_log.Multiline = true;
+			this.textBox_log.Name = "textBox_log";
+			this.textBox_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox_log.Size = new System.Drawing.Size(716, 220);
+			this.textBox_log.TabIndex = 6;
+			this.textBox_log.Text = resources.GetString("textBox_log.Text");
+			this.textBox_log.WordWrap = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.AutoScroll = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(757, 345);
+			this.Controls.Add(this.textBox_log);
+			this.Controls.Add(this.textBox_code);
+			this.Controls.Add(this.textBox_url);
+			this.Controls.Add(this.啊);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "1024上岸神器 by.蜗牛快快跑 个人小站www.mtils.com ";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormCloseing);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Register1024/MainForm.cs
@@ -0,0 +1,286 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2019/1/15
+ * Time: 13:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace Register1024
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void 关于ToolStripMenuItem1Click(object sender, EventArgs e)
+		{
+			String tips = "写这个软件其实在去年2018.8月份就有想法了，但由于其他事一直没做.\r\n" +
+				"个人小站,请大家多多支持;www.mtils.com" +
+				"写个这个帮助大家上岸吧.其实我也是用的这个^_^上的岸。QQ.1070509085希望结交更多的朋友";
+			MessageBox.Show(tips);
+			
+		}
+		void 使用方法ToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			String tips = "caoliu是一个需要邀请码注册的轮胎; \r\n" +
+				"有的好人直接发邀请码，大家拿着可以直接注册,这种不用讲了\r\n" +
+				"还有一类人不喜欢直接发邀请码, 而是隐藏其中几位再发。\r\n" +
+				"大家得到后不能直接注册,要先补全后再注册\r\n" +
+				"通常来说隐藏2个字符就有多达81种邀请码组合\r\n" +
+				"手工补全后再注册的成功率是可以想象的，抢过的自然明白^_^。所以\r\n" +
+				"本工具原理就是通过规则去生成所有可能的注册码,然后用机器逐一去试。\r\n" +
+				"使用方法:\r\n" +
+				"首先得拿到一个暗码，比如：cbf1c96e6894*7*9\r\n" +
+				"这里假如发码好人隐藏了两位：可以看到有两个星号，然后他会说这两个分别是数字和字母\r\n" +
+				"那么我们就可以使用 {i} 代替数字的星号, {c} 代替字母的星号\r\n" +
+				"替换结果为：cbf1c96e6894{i}7{c}9 \r\n" +
+				"将替换的结果填到软件邀请码位置,然后等待计算结果就行。\r\n" +
+				"可能有的人要问，如果不知道是字符还是字母怎么办， \r\n" +
+				"其实我们还有一个 {a} ,它可以替换那些你不知道具体是什么的星号\r\n" +
+				"" ;
+			MessageBox.Show(tips);
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			List<String> res = initIncrement(textBox_code.Text);
+
+			if(null == res || 0 == res.Count) {
+				addLog("输入的邀请码解析错误或者需要尝试的组合为0, 请先查看 使用手册->使用方法");
+				return;
+			} 
+			
+			addLog(string.Format("破解开始,共 {0} 种组合需要尝试，预估耗时 {1} s",  res.Count, (res.Count * 10)));
+			
+			this.button1.Enabled = false;
+			//方法一：使用Thread类
+	        ParameterizedThreadStart threadStart = new ParameterizedThreadStart(threadEntry);//通过ThreadStart委托告诉子线程执行什么方法　　
+	        Thread thread = new Thread(threadStart);
+	        thread.Start(res);//启动新线程
+          
+			
+		}
+		
+	
+		//线程入口
+		void threadEntry(Object o) {
+			List<String> res = (List<String>) o;
+			Random r1 = new Random();
+			for(int i=0; i<res.Count; i++) {
+				check(res[i]);
+				if(i == res.Count - 1) {
+					addLog("end");
+				}
+				Thread.Sleep(r1.Next(5000, 15000));
+			}
+		}
+		
+		//检查邀请码是否有效
+		void check(String code) {
+			
+			String param = String.Format("reginvcode={0}&action=reginvcodeck", code);
+			String res = GetContentPost(textBox_url.Text + "/register.php?", param, Encoding.ASCII);
+			if(null == res) return;
+			Regex rg = new Regex("<script language=\"JavaScript1.2\">(.*?)</script>", RegexOptions.Multiline | RegexOptions.Singleline);
+			if(-1 != res.IndexOf("parent.retmsg_invcode('0')")) {
+				addLog(String.Format("{0}, {1}", code, "-->> 有效"));
+				Application.ExitThread();
+			} else {
+				addLog(String.Format("{0}, {1}, {2}", code, " 无效", rg.Match(res).Groups[1].Value));
+			}
+		}
+		
+		//提交数据
+        public string GetContentPost(string uri, string data, Encoding coding)
+        {
+        	try{
+        		HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
+	            request.ContentType = "application/x-www-form-urlencoded";
+	            request.Method = "POST";
+	            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
+	                        
+	            //将字符串数据转化为字节串,这也是POST请求与GET请求区别的地方
+	            byte[] buffer = coding.GetBytes(data);
+	            request.ContentLength = buffer.Length;
+	            
+	            //用于将数据写入Internet资源
+	            Stream stream = request.GetRequestStream();
+	            stream.Write(buffer, 0, buffer.Length);
+	
+	            WebResponse response = request.GetResponse();
+	
+	            //从网络资源中返回数据流
+	            stream = response.GetResponseStream();
+	
+	            StreamReader sr = new StreamReader(stream, coding);
+	
+	            //将数据流转换文字符串
+	            string result = sr.ReadToEnd();
+	
+	            //关闭流数据
+	            stream.Close();
+	            sr.Close();
+	
+	            return result;
+        	} catch(Exception  e) {
+        		addLog(String.Format("网络异常,请检查网络状况:{0}", e.Message));
+        		addLog("end");
+        		Application.ExitThread();
+        		return null;
+        	}
+        }
+	
+        
+        //自增时初始化环境
+		List<String> initIncrement(String t) {
+        	List<String> jw = new List<string>();
+        	String target = t;
+			
+			Regex rg = new Regex("(\\{i\\}|\\{a\\}|\\{c\\})", RegexOptions.Multiline | RegexOptions.Singleline);    	
+			MatchCollection  mc = rg.Matches(t);
+			foreach (Match match in mc) {
+				jw.Add( match.Groups[0].Value);
+			}
+			
+			foreach(String tmp in jw) {
+				if(tmp.Equals("{c}")) {
+					target = target.Replace("{c}", "f");
+				}
+				if(tmp.Equals("{a}")) {
+					target = target.Replace("{a}", "f");
+				}
+				if(tmp.Equals("{i}")) {
+					target = target.Replace("{i}", "9");
+				}
+			}
+			
+
+			
+			if(3 < jw.Count) {
+				addLog("使用的匹配符不能超出3个,正常情况超出三个尝试时间将会大于5小时");
+				return null;
+			}
+			
+			return increment(t, target, jw, null, null);
+		
+			
+		}
+        
+        
+        
+        //模拟计数器
+        //占位符模拟依次递增,直到满足条件退出
+        List<String> increment(String code, String target, List<String> jw, List<Char> last, List<string> res) {
+        	if(null == res) res = new List<string>();
+        	if(null == last) {
+        		last = new List<Char>();
+        		foreach(String tmp in jw) {
+        			if(tmp.Equals("{c}")) {
+        				last.Add('a');
+        			} else {
+	        			last.Add('0');
+        			}
+        		}
+        	}
+        	
+    		String tmp2 = code;
+        	for(int i =0; i<jw.Count; i++) {
+        		if(jw[i].Equals("{c}")) {
+        			tmp2 = tmp2.Replace("{c}", last[i].ToString());
+				}
+				if(jw[i].Equals("{a}")) {
+					tmp2 = tmp2.Replace("{a}", last[i].ToString());
+				}
+				if(jw[i].Equals("{i}")) {
+        			tmp2 = tmp2.Replace("{i}", last[i].ToString());
+				}
+			}
+        	res.Add(tmp2);
+        	
+        	if(target.Equals(tmp2)) return res;
+        	
+        	
+        	last[0] = (char)((int)(last[0]) + 1);
+        	
+        	for(int i =0; i<jw.Count; i++) {
+        		String tmp = jw[i];
+        		
+				if(tmp.Equals("{c}")) {
+        			if('f' < last[i]) {
+        				last[i] = 'a';
+        				if(i != jw.Count - 1) last[i+1] = (char)((int)(last[i+1]) + 1);
+        			}
+				}
+				if(tmp.Equals("{a}")) {
+        			if('9' < last[i] && 'a' > last[i]) {
+        				last[i] = 'a';
+        			} else if('f' < last[i]) {
+        				last[i] = '0';
+    					if(i != jw.Count - 1)  last[i+1] = (char)((int)(last[i+1]) + 1);
+        			}
+				}
+
+				if(tmp.Equals("{i}")) {
+					if('9' < last[i]) {
+        				last[i] = '0';
+        				if(i != jw.Count - 1)  last[i+1] = (char)((int)(last[i+1]) + 1);
+        			}
+				}
+			}
+        	
+        
+        	return increment(code, target, jw, last, res);
+        }
+	
+        //输出日志
+		void addLog(String t) {
+        	if(t.Equals("end")) {
+        		this.button1.BeginInvoke(new Action(()=>{
+                            	this.button1.Enabled = true;
+            	}));
+        		addLog("本次任务结束");
+        		return;
+        	}
+        	textBox_log.BeginInvoke(new Action(()=>
+            {
+               textBox_log.AppendText(string.Format("{0}: {1}\r\n",  DateTime.Now, t));
+            }));
+			
+		}
+        
+        
+        //程序退出
+		void MainFormCloseing(object sender, EventArgs e)
+		{
+			System.Environment.Exit(0);
+		}
+	
+		
+	
+	}
+	
+	
+}
